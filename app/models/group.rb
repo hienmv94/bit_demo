b59893490eb2159a9c2a324f65bf01f98bef6476
Mai Van Hien <hienmv94@gmail.com>
2016-03-07 17:09:11 +0700

    create group with member

---
+++ app/models/group.rb
@@ -0,0 +1,9 @@
+class Group < ActiveRecord::Base
+	belongs_to :user
+	
+	has_many :members, dependent: :destroy
+	has_many :assignments
+	has_many :member_assignments
+
+	accepts_nested_attributes_for :members, allow_destroy: true
+end
